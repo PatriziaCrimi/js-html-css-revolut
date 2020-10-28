6240c5bba214f5bbefa26ef676ae82ed9cbd179c
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-28 18:44:00 +0100

    Solution 2 - with "click" (bug to be fixed)

--- action.js
+++ action.js
@@ -1,9 +1,11 @@
+// ---------- SOLUTION 1: MOUSEENTER & MOUSELEAVE ----------
+
 $(document).ready(function() {
-  // Hover on an item in the navbar menu (select it)
+  // Hover on a dropdown item in the navbar menu (to select it)
   $('.dropdown-item').mouseenter(function() {
     // Finding and closing any already open dropdown menu
     $('.active').removeClass('active');
-    // Finding the dropdown menu corresponding to the dropdown item I've clicked
+    // Finding the dropdown menu corresponding to the dropdown item
     var current_dropdown_menu = $(this).find('.dropdown-menu');
     // Opening the corresponding dropdown menu
     current_dropdown_menu.addClass('active');
@@ -15,3 +17,25 @@ $(document).ready(function() {
     $(this).removeClass('active');
   });
 });
+
+/*
+// ---------- SOLUTION 2: CLICK ----------
+$(document).ready(function() {
+  // Clicking on a dropdown item in the navbar menu (to select it)
+  $('.dropdown-item').click(function() {
+
+
+    BUG TO BE FIXED: senza questa istruzione quando clicco su una nuova voce di menu non  si chiudono automaticamente gli altri menu aperti
+    MA se metto questa istruzione non funziona il toggle perché trova sempre l'elemento privo di classe e quindi gliela aggiunge sempre (e l'elemento cliccato non si richiude mai!!!! Resta aperto)
+
+    // Finding and closing any already open dropdown menu
+    $('.active').removeClass('active');
+
+
+    // Finding the dropdown menu corresponding to the dropdown item
+    var current_dropdown_menu = $(this).find('.dropdown-menu');
+    // Opening & closing the corresponding dropdown menu
+    current_dropdown_menu.toggleClass('active');
+  });
+});
+*/
