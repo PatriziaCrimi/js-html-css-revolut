1e2c352729f18e145f0ada7f335aced4bdcf6bdd
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-28 16:55:41 +0100

    Basic algorithm in JS with mouseenter & mouseleave completed

--- action.js
+++ action.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+  // Hover on an item in the navbar menu (select it)
+  $('.dropdown-item').mouseenter(function() {
+    // Opening the corresponding dropdown item
+    $('.dropdown-menu').addClass('active');
+  });
 
+  // Leaving the item in the navbar menu (deselect it)
+  $('.dropdown-menu').mouseleave(function() {
+    // Closing the corresponding dropdown item
+    $('.dropdown-menu').removeClass('active');
+  });
+});
