0eb229be79dc21d2b86c19907e9c3c3685612175
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-28 17:46:03 +0100

    Exercise completed - Solution 1 with "mouseenter & mouseleave"

--- action.js
+++ action.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   // Hover on an item in the navbar menu (select it)
   $('.dropdown-item').mouseenter(function() {
+    // Finding and closing any already open dropdown menu
+    $('.active').removeClass('active');
+    // Finding the dropdown menu corresponding to the dropdown item I've clicked
+    var current_dropdown_menu = $(this).find('.dropdown-menu');
     // Opening the corresponding dropdown menu
-    $(this' > .dropdown-menu').addClass('active');
+    current_dropdown_menu.addClass('active');
   });
 
   // Leaving the item in the navbar menu (deselect it)
   $('.dropdown-menu').mouseleave(function() {
     // Closing the corresponding dropdown item
-    $('.dropdown-menu').removeClass('active');
+    $(this).removeClass('active');
   });
 });
